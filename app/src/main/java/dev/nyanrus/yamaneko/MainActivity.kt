6ce9e09a45bf63c088f201e8a125145668679f35
NyanRus <68762426+nyanrus@users.noreply.github.com>
2024-02-28 23:25:48 +0900

    use middleware and add searchbar

--- app/src/main/java/dev/nyanrus/yamaneko/MainActivity.kt
+++ app/src/main/java/dev/nyanrus/yamaneko/MainActivity.kt
@@ -4,48 +4,70 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TextButton
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
 import dev.nyanrus.yamaneko.ui.theme.YamanekoTheme
 import mozilla.components.browser.engine.gecko.GeckoEngine
-import mozilla.components.browser.engine.gecko.NestedGeckoView
-import mozilla.components.concept.engine.EngineSession
-import mozilla.components.concept.engine.EngineView
-import org.mozilla.geckoview.GeckoRuntime
-import org.mozilla.geckoview.GeckoRuntimeSettings
-import org.mozilla.geckoview.GeckoSession
-import org.mozilla.geckoview.GeckoView
+import mozilla.components.browser.state.engine.EngineMiddleware
+import mozilla.components.browser.state.store.BrowserStore
+import mozilla.components.compose.engine.WebContent
+import mozilla.components.feature.search.middleware.SearchMiddleware
+import mozilla.components.feature.search.region.RegionMiddleware
+import mozilla.components.feature.session.SessionUseCases
+import mozilla.components.service.location.LocationService
+import mozilla.components.browser.state.helper.Target
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val builder = GeckoRuntimeSettings.Builder()
-        val runtimeSettings = builder
-            .aboutConfigEnabled(true)
-            .build()
-        val runtime = GeckoRuntime.create(applicationContext, runtimeSettings)
+        //val builder = GeckoRuntimeSettings.Builder()
+//        val runtimeSettings = builder
+//            .aboutConfigEnabled(true)
+//            .build()
+        //val runtime = GeckoRuntime.create(applicationContext, runtimeSettings)
 
-        val engine = GeckoEngine(applicationContext,null,runtime)
-        val session = engine.createSession(false)
-        session.loadUrl("qiita.com")
+        val engine = GeckoEngine(applicationContext)
+
+//        val session = engine.createSession(false)
+//        session.loadUrl("qiita.com")
+
+        val locationService by lazy { LocationService.default() }
+
+        val store by lazy {
+            BrowserStore(
+                middleware = listOf(
+                    RegionMiddleware(applicationContext, locationService),
+                    SearchMiddleware(applicationContext),
+                ) + EngineMiddleware.create(engine),
+            )
+        }
+
+        val session = SessionUseCases(store)
+        session.loadUrl("google.com")
 
         setContent {
             YamanekoTheme {
@@ -55,23 +77,20 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Scaffold(bottomBar = {
-                        BottomAppBar(modifier=Modifier.height(80.dp)) {
-                            Box {
-                                Text("hello")
-                            }
-                        }
+                        BottomBar(store,Target.SelectedTab, Modifier.height(80.dp))
                     },
                     ) { innerPadding ->
                         Box(Modifier.padding(innerPadding)) {
-                            AndroidView(
-                                factory = { context ->
-                                    engine.createView(context).asView()
-                                },
-                                update = { context ->
-                                    val engineView = context as EngineView
-                                    engineView.render(session)
-                                }
-                            )
+                            WebContent(engine = engine, store = store, target = Target.SelectedTab)
+//                            AndroidView(
+//                                factory = { context ->
+//                                    engine.createView(context).asView()
+//                                },
+//                                update = { context ->
+//                                    val engineView = context as EngineView
+//                                    engineView.render(session)
+//                                }
+//                            )
                         }
                     }
                 }
@@ -81,17 +100,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun BottomBar(store: BrowserStore,target: Target,modifier: Modifier = Modifier) {
+    BottomAppBar(modifier=modifier) {
+        Row {
+            SearchBar(store,target)
+            ReloadButton(store)
+        }
+    }
+}
+
+
+@Composable
+fun SearchBar(store: BrowserStore,target: Target) {
+    //https://searchfox.org/mozilla-mobile/rev/1dc5f1289afb580e32b66303a78ab5d92ce30ca5/firefox-android/android-components/components/compose/engine/src/main/java/mozilla/components/compose/engine/WebContent.kt#27
+    val selectedTab = target.observeAsComposableStateFrom(store, observe = { tab ->
+        tab?.content?.url
+    })
+    //もうちょっと後に持ってきたらhttpsみたいなURL得られるけどめんどい
+    var text by remember { mutableStateOf(selectedTab.value!!.content.url) }
+
+    //https://stackoverflow.com/questions/59133100/how-to-close-the-virtual-keyboard-from-a-jetpack-compose-textfield
+    val focusManager = LocalFocusManager.current
+    val session = SessionUseCases(store)
+    OutlinedTextField(
+        value = text,
+        onValueChange = { text = it },
+        keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Search),
+        keyboardActions = KeyboardActions(onSearch = {
+            session.loadUrl(text)
+            focusManager.clearFocus()
+        }),
+        maxLines = 1
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    YamanekoTheme {
-        Greeting("Android")
+fun ReloadButton(store: BrowserStore) {
+    val session = SessionUseCases(store)
+    TextButton(onClick = {session.reload()}) {
+        Icon(
+            imageVector = Icons.Default.Refresh,
+            contentDescription = "Reload"
+        )
     }
 }
